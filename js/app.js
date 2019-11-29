09eb1d3791ff491dcd55b253564b80d3f31f59a0
Kieran Barker <kieran11397@gmail.com>
2019-11-29 18:24:26 +0000

    Add each form field to the global data object as a property

--- js/app.js
+++ js/app.js
@@ -2,4 +2,39 @@
 
   "use strict";
 
+  //
+  // Variables
+  //
+
+  var form = d.querySelector("form");
+  var fields = Array.prototype.slice.call(form.elements);
+  var data = {};
+
+
+  //
+  // Functions
+  //
+
+  /**
+   * Add a form field to the global data object as a property
+   */
+  function addPropertyToData(field) {
+    if (!field || !field.hasAttribute("name")) return;
+    data[field.getAttribute("name")] = field.value;
+  }
+
+  // function setData() {}
+
+  //
+  // Init
+  //
+
+  // Add each form field to the global data object as a property
+  fields.forEach(addPropertyToData);
+
+  // form.addEventListener("input", function(event) {}, false);
+  // form.addEventListener("submit", function(event) {}, false);
+
+  console.log(data);
+
 })(document);
